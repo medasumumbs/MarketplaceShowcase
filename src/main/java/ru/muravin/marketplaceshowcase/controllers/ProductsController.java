ada55cc662889591109ce1f257086722e8321ab7
sem <sem@finstream.ru>
2025-03-03 13:01:16 +0300

    Реализована заготовка CSV парсера

--- src/main/java/ru/muravin/marketplaceshowcase/controllers/ProductsController.java
+++ src/main/java/ru/muravin/marketplaceshowcase/controllers/ProductsController.java
@@ -1,14 +1,37 @@
 package ru.muravin.marketplaceshowcase.controllers;
 
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import com.opencsv.exceptions.CsvValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+import ru.muravin.marketplaceshowcase.models.Product;
 import ru.muravin.marketplaceshowcase.services.ProductsService;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.Iterator;
+import java.util.List;
 
 @Controller
 @RequestMapping("/products")
@@ -37,4 +60,60 @@ public class ProductsController {
 
         return modelAndView;
     }
+
+
+
+    @GetMapping("/uploadCSV")
+    public String uploadCSV() {
+        return "uploadCSV";
+    }
+
+    @PostMapping(
+            value = "/uploadCSV",
+            consumes = "multipart/form-data" // Обязательно включаем медиа-тип
+    )
+    public String uploadIcon(
+            @RequestPart("csv") MultipartFile csvFile, // Файл в виде массива байт
+            Model model) throws IOException, CsvValidationException {
+        if (csvFile.isEmpty()) {
+            model.addAttribute("message", "Файл пуст, импорт не выполнен");
+            return "uploadCSVStatus";
+        }
+        try (CSVReader csvReader = new CSVReaderBuilder(
+                new BufferedReader(
+                    new InputStreamReader(csvFile.getInputStream())
+                )
+            ).withCSVParser(
+                new CSVParserBuilder().withSeparator(',').build()
+            ).build()) {
+
+            ColumnPositionMappingStrategy<Product> beanStrategy = new ColumnPositionMappingStrategy<Product>();
+            beanStrategy.setType(Product.class);
+            beanStrategy.setColumnMapping(new String[] {"id","name","description","price","imageBase64"});
+
+            String[] header = csvReader.readNext();
+            System.out.println(header[0]);
+
+            CsvToBean<Product> csvToBean = new CsvToBean<Product>();
+            csvToBean.setMappingStrategy(beanStrategy);
+            csvToBean.setCsvReader(csvReader);
+            List<Product> products = csvToBean.parse();
+            products.forEach(product -> {
+                System.out.println(product.toString());
+            });
+
+            // save users list on model
+            model.addAttribute("message", "");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw ex;
+        }
+        return "uploadCSVStatus";
+    }
+    @ExceptionHandler(Exception.class)
+    public String errorPage(Model model, Exception exception) {
+        model.addAttribute("message", "Ошибка импорта - проверьте файл на корректность: " + exception.getCause());
+        return "errorPage";
+    }
+
 }
