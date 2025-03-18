b52130a5cc3676dbfef38dd72c832580641f9548
sem <sem@finstream.ru>
2025-03-18 15:35:43 +0300

    завершены тесты для службы productservice

--- src/test/java/ru/muravin/marketplaceshowcase/serviceTests/ProductServiceTest.java
+++ src/test/java/ru/muravin/marketplaceshowcase/serviceTests/ProductServiceTest.java
@@ -12,12 +12,15 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import ru.muravin.marketplaceshowcase.MarketplaceShowcaseApplication;
 import ru.muravin.marketplaceshowcase.TestcontainersConfiguration;
 import ru.muravin.marketplaceshowcase.dto.ProductToUIDto;
+import ru.muravin.marketplaceshowcase.exceptions.UnknownProductException;
 import ru.muravin.marketplaceshowcase.models.Product;
 import ru.muravin.marketplaceshowcase.repositories.ProductsRepository;
 import ru.muravin.marketplaceshowcase.services.CartService;
 import ru.muravin.marketplaceshowcase.services.ProductsService;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -65,5 +68,50 @@ public class ProductServiceTest {
         verifyNoMoreInteractions(repository);
         verify(cartService, times(1)).getCartItems(1l);
         verifyNoMoreInteractions(cartService);
+    }
+    @Test
+    void testSave() {
+        var product = new Product(0l,"iphone",25d,"desc",new byte[0]);
+        var dto = new ProductToUIDto(product);
+        productsService.save(dto);
+        verify(repository, times(1)).save(product);
+    }
+    @Test
+    void testFindById() {
+        var product = new Product(1L,"iphone",25d,"desc",new byte[0]);
+        when(repository.findById(1L)).thenReturn(Optional.of(product));
+        var result = productsService.findById(1L);
+        assertNotNull(result);
+        var dto = new ProductToUIDto(product);
+        dto.setQuantityInCart(0);
+        result.setQuantityInCart(0);
+        assertEquals(dto, result);
+    }
+    @Test
+    void testFindByIdNotFound() {
+        when(repository.findById(1L)).thenReturn(Optional.empty());
+        assertThrows(UnknownProductException.class, () -> productsService.findById(1L));
+    }
+    @Test
+    void testFindByNameLike() {
+        var pageable = PageRequest.of(1,2);
+        List<Product> products = List.of(
+                new Product(0l,"iphone",25d,"desc",new byte[0]),
+                new Product(1l,"iphone",25d,"desc",new byte[0]),
+                new Product(2l,"iphone 2",26d,"desc",new byte[0]),
+                new Product(3l,"iphone 2",27d,"desc",new byte[0])
+        );;
+        var searchString = "iphone";
+        when(repository.findByNameLike("%"+searchString+"%",pageable)).thenReturn(products.subList(1,3));
+        when(cartService.getCartItems(1L)).thenReturn(new ArrayList<>());
+        var result = productsService.findByNameLike(searchString, pageable);
+        assertNotNull(result);
+        verify(repository, times(1)).findByNameLike("%"+searchString+"%", pageable);
+        verifyNoMoreInteractions(repository);
+        var expected = products.subList(1,3).stream().map(ProductToUIDto::new).toList();
+        expected.forEach(
+                a -> a.setQuantityInCart(0)
+        );
+        assertEquals(expected, result);
     }
 }
