d3e3713718621ee551609090e4441b6669831f4c
medasu <semen.muravin@gmail.com>
2025-04-01 17:44:30 +0300

    Исправлен первый интеграционный тест внутри CartControllerTest

--- src/main/java/ru/muravin/marketplaceshowcase/models/User.java
+++ src/main/java/ru/muravin/marketplaceshowcase/models/User.java
@@ -19,7 +19,5 @@ public class User {
     private String password;
 
     private String email;
-
-    private Long cartId;
 }
 
--- src/test/java/ru/muravin/marketplaceshowcase/integrationTests/CartControllerTest.java
+++ src/test/java/ru/muravin/marketplaceshowcase/integrationTests/CartControllerTest.java
@@ -4,75 +4,74 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.context.WebApplicationContext;
 import ru.muravin.marketplaceshowcase.MarketplaceShowcaseApplication;
 import ru.muravin.marketplaceshowcase.TestcontainersConfiguration;
 import ru.muravin.marketplaceshowcase.models.Cart;
 import ru.muravin.marketplaceshowcase.models.Product;
 import ru.muravin.marketplaceshowcase.models.User;
-import ru.muravin.marketplaceshowcase.repositories.CartItemsReactiveRepository;
-import ru.muravin.marketplaceshowcase.repositories.CartsReactiveRepository;
+import ru.muravin.marketplaceshowcase.repositories.*;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest(classes = MarketplaceShowcaseApplication.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = MarketplaceShowcaseApplication.class)
 @Import(TestcontainersConfiguration.class)
 @TestPropertySource(locations = "classpath:application.yml")
-@Disabled
+@AutoConfigureWebTestClient
 public class CartControllerTest {
     @Autowired
-    private WebApplicationContext webApplicationContext;
-
+    WebTestClient webTestClient;
     @Autowired
     private CartsReactiveRepository cartsReactiveRepository;
 
     @Autowired
     private CartItemsReactiveRepository cartItemsReactiveRepository;
-  /*  @Autowired
-    CartItemRepository cartItemRepository;
     @Autowired
-    OrderItemRepository orderItemRepository;
+    OrderItemsReactiveRepository orderItemsReactiveRepository;
     @Autowired
-    OrderRepository orderRepository;
+    OrderReactiveRepository orderReactiveRepository;
     @Autowired
-    ProductsRepository productsRepository;*/
-
-    private MockMvc mockMvc; // Используется для отправки HTTP-запросов
+    ProductsReactiveRepository productsReactiveRepository;
+    @Autowired
+    private UserReactiveRepository userReactiveRepository;
 
     @BeforeEach
     void setUp() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-        /*cartItemRepository.deleteAll();
-        orderItemRepository.deleteAll();
-        productsRepository.deleteAll();
-        orderRepository.deleteAll();*/
-        //userRepository.deleteAll();
-        //cartsRepository.deleteAll();
+        cartItemsReactiveRepository.deleteAll().block();
+        orderItemsReactiveRepository.deleteAll().block();
+        productsReactiveRepository.deleteAll().block();
+        orderReactiveRepository.deleteAll().block();
+        cartsReactiveRepository.deleteAll().block();
+        userReactiveRepository.deleteAll().block();
         var user = new User();
         user.setUsername("username");
         user.setPassword("password");
         user.setEmail("abc@gmail.com");
         var cart = new Cart();
-        //userRepository.save(user);
-        //cart.setUser(user);
-        //cartsRepository.save(cart);
-
+        user = userReactiveRepository.save(user).block();
+        cart.setUserId(user.getId());
+        cartsReactiveRepository.save(cart).block();
         var product = new Product(1L,"iphone",25d,"desc",new byte[0]);
         product.setId(null);
-        //productsRepository.save(product);
+        productsReactiveRepository.save(product).block();
     }
     @Test
     void addItemToCartTest() throws Exception {
-        mockMvc.perform(post("/cart/add").param("productId","1")).andExpect(status().is3xxRedirection())
-                .andExpect(redirectedUrl("/products"));
+        var product = productsReactiveRepository.findAll().blockFirst();
+        webTestClient.post().uri("/products/changeCartItemQuantity/"+product.getId()).exchange().expectStatus().is3xxRedirection().expectHeader().location("/products");
     }
     @Test
     void showCartTest() throws Exception {
