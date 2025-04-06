a8d90071a8ac7fc6083b6663a4d3ec6ec35fe146
medasu <semen.muravin@gmail.com>
2025-04-06 17:38:21 +0300

    Реализовано обращение из основного приложения к сервису платежей при просмотре корзины

--- marketplaceApplication/src/main/java/ru/muravin/marketplaceshowcase/controllers/CartController.java
+++ marketplaceApplication/src/main/java/ru/muravin/marketplaceshowcase/controllers/CartController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import org.springframework.web.reactive.result.view.Rendering;
 import reactor.core.publisher.Mono;
+import ru.muravin.marketplaceshowcase.paymentServiceClient.api.PaymentServiceClientAPI;
 import ru.muravin.marketplaceshowcase.services.CartService;
 
 import java.net.URI;
@@ -18,9 +19,11 @@ import java.util.HashMap;
 @RequestMapping("/cart")
 public class CartController {
     private final CartService cartService;
+    private final PaymentServiceClientAPI paymentServiceClientAPI;
 
-    public CartController(CartService cartService) {
+    public CartController(CartService cartService, PaymentServiceClientAPI paymentServiceClientAPI) {
         this.cartService = cartService;
+        this.paymentServiceClientAPI = paymentServiceClientAPI;
     }
 
     @PostMapping("/add")
@@ -32,10 +35,17 @@ public class CartController {
         var firstCartIdMono = cartService.getFirstCartIdMono();
         var sumOfOrder = cartService.getCartSumFlux(firstCartIdMono);
         var cartItems = cartService.getCartItemsDtoFlux(firstCartIdMono);
-        return Mono.zip(sumOfOrder, cartItems.collectList()).map(tuple -> {
+        var balance = paymentServiceClientAPI.usersUserIdGet(1);
+        return Mono.zip(sumOfOrder, cartItems.collectList(), balance).map(tuple -> {
             var hashMap = new HashMap<String, Object>();
             hashMap.put("cartItems", tuple.getT2());
             hashMap.put("sumOfOrder", tuple.getT1());
+            hashMap.put("balance", tuple.getT3().getBalance());
+            if (tuple.getT3().getBalance() < 0) {
+                hashMap.put("serviceIsUnavailable", true);
+            } else {
+                hashMap.put("serviceIsUnavailable", false);
+            }
             return Rendering.view("cart").model(hashMap).build();
         });
     }
---
+++ marketplaceApplication/src/main/java/ru/muravin/marketplaceshowcase/paymentServiceClient/api/PaymentServiceClientAPI.java
@@ -0,0 +1,242 @@
+package ru.muravin.marketplaceshowcase.paymentServiceClient.api;
+
+import org.springframework.stereotype.Component;
+import ru.muravin.marketplaceshowcase.paymentServiceClient.server.ApiClient;
+
+import ru.muravin.marketplaceshowcase.paymentServiceClient.model.Balance;
+import ru.muravin.marketplaceshowcase.paymentServiceClient.model.PaymentResponse;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.web.reactive.function.client.WebClient.ResponseSpec;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import reactor.core.publisher.Mono;
+
+@Component
+public class PaymentServiceClientAPI {
+
+    private ApiClient apiClient;
+
+    public PaymentServiceClientAPI() {
+        this(new ApiClient());
+    }
+
+    @Autowired
+    public PaymentServiceClientAPI(ApiClient apiClient) {
+        this.apiClient = apiClient;
+    }
+
+    public ApiClient getApiClient() {
+        return apiClient;
+    }
+
+    public void setApiClient(ApiClient apiClient) {
+        this.apiClient = apiClient;
+    }
+
+    
+    /**
+     * Получить баланс пользователя
+     * 
+     * <p><b>200</b> - Успешный ответ
+     * <p><b>404</b> - Пользователь не найден
+     * <p><b>5XX</b> - Внутренняя ошибка сервера
+     * <p><b>0</b> - Неизвестная ошибка при выяснении баланса
+     * @param userId Идентификатор пользователя
+     * @return Balance
+     * @throws WebClientResponseException if an error occurs while attempting to invoke the API
+     */
+    private ResponseSpec usersUserIdGetRequestCreation(Integer userId) throws WebClientResponseException {
+        Object postBody = null;
+        // verify the required parameter 'userId' is set
+        if (userId == null) {
+            throw new WebClientResponseException("Missing the required parameter 'userId' when calling usersUserIdGet", HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase(), null, null, null);
+        }
+        // create path and map variables
+        final Map<String, Object> pathParams = new HashMap<String, Object>();
+
+        pathParams.put("userId", userId);
+
+        final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<String, String>();
+        final HttpHeaders headerParams = new HttpHeaders();
+        final MultiValueMap<String, String> cookieParams = new LinkedMultiValueMap<String, String>();
+        final MultiValueMap<String, Object> formParams = new LinkedMultiValueMap<String, Object>();
+
+        final String[] localVarAccepts = { 
+            "application/json"
+        };
+        final List<MediaType> localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
+        final String[] localVarContentTypes = { };
+        final MediaType localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
+
+        String[] localVarAuthNames = new String[] {  };
+
+        ParameterizedTypeReference<Balance> localVarReturnType = new ParameterizedTypeReference<Balance>() {};
+        return apiClient.invokeAPI("/users/{userId}", HttpMethod.GET, pathParams, queryParams, postBody, headerParams, cookieParams, formParams, localVarAccept, localVarContentType, localVarAuthNames, localVarReturnType);
+    }
+
+    /**
+     * Получить баланс пользователя
+     * 
+     * <p><b>200</b> - Успешный ответ
+     * <p><b>404</b> - Пользователь не найден
+     * <p><b>5XX</b> - Внутренняя ошибка сервера
+     * <p><b>0</b> - Неизвестная ошибка при выяснении баланса
+     * @param userId Идентификатор пользователя
+     * @return Balance
+     * @throws WebClientResponseException if an error occurs while attempting to invoke the API
+     */
+    public Mono<Balance> usersUserIdGet(Integer userId) throws WebClientResponseException {
+        ParameterizedTypeReference<Balance> localVarReturnType = new ParameterizedTypeReference<Balance>() {};
+        var emptyBalance = new Balance();
+        emptyBalance.setBalance(0f);
+        var errorBalance = new Balance();
+        errorBalance.setBalance(-1f);
+        return usersUserIdGetRequestCreation(userId).bodyToMono(localVarReturnType)
+                .onErrorReturn(errorBalance)
+                .switchIfEmpty(Mono.just(emptyBalance));
+    }
+
+    /**
+     * Получить баланс пользователя
+     * 
+     * <p><b>200</b> - Успешный ответ
+     * <p><b>404</b> - Пользователь не найден
+     * <p><b>5XX</b> - Внутренняя ошибка сервера
+     * <p><b>0</b> - Неизвестная ошибка при выяснении баланса
+     * @param userId Идентификатор пользователя
+     * @return ResponseEntity&lt;Balance&gt;
+     * @throws WebClientResponseException if an error occurs while attempting to invoke the API
+     */
+    public Mono<ResponseEntity<Balance>> usersUserIdGetWithHttpInfo(Integer userId) throws WebClientResponseException {
+        ParameterizedTypeReference<Balance> localVarReturnType = new ParameterizedTypeReference<Balance>() {};
+        return usersUserIdGetRequestCreation(userId).toEntity(localVarReturnType);
+    }
+
+    /**
+     * Получить баланс пользователя
+     * 
+     * <p><b>200</b> - Успешный ответ
+     * <p><b>404</b> - Пользователь не найден
+     * <p><b>5XX</b> - Внутренняя ошибка сервера
+     * <p><b>0</b> - Неизвестная ошибка при выяснении баланса
+     * @param userId Идентификатор пользователя
+     * @return ResponseSpec
+     * @throws WebClientResponseException if an error occurs while attempting to invoke the API
+     */
+    public ResponseSpec usersUserIdGetWithResponseSpec(Integer userId) throws WebClientResponseException {
+        return usersUserIdGetRequestCreation(userId);
+    }
+
+    /**
+     * Совершить платеж (совершить списание денег со счёта)
+     * 
+     * <p><b>200</b> - Успешный ответ
+     * <p><b>400</b> - Превышена максимально возможная сумма платежа
+     * <p><b>404</b> - Пользователь не найден
+     * <p><b>5XX</b> - Внутренняя ошибка сервера
+     * <p><b>0</b> - Неизвестная ошибка при выполнении платежа
+     * @param sum The sum parameter
+     * @param userId Идентификатор пользователя
+     * @return PaymentResponse
+     * @throws WebClientResponseException if an error occurs while attempting to invoke the API
+     */
+    private ResponseSpec usersUserIdMakePaymentPostRequestCreation(Float sum, Integer userId) throws WebClientResponseException {
+        Object postBody = null;
+        // verify the required parameter 'sum' is set
+        if (sum == null) {
+            throw new WebClientResponseException("Missing the required parameter 'sum' when calling usersUserIdMakePaymentPost", HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase(), null, null, null);
+        }
+        // verify the required parameter 'userId' is set
+        if (userId == null) {
+            throw new WebClientResponseException("Missing the required parameter 'userId' when calling usersUserIdMakePaymentPost", HttpStatus.BAD_REQUEST.value(), HttpStatus.BAD_REQUEST.getReasonPhrase(), null, null, null);
+        }
+        // create path and map variables
+        final Map<String, Object> pathParams = new HashMap<String, Object>();
+
+        pathParams.put("userId", userId);
+
+        final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<String, String>();
+        final HttpHeaders headerParams = new HttpHeaders();
+        final MultiValueMap<String, String> cookieParams = new LinkedMultiValueMap<String, String>();
+        final MultiValueMap<String, Object> formParams = new LinkedMultiValueMap<String, Object>();
+
+        queryParams.putAll(apiClient.parameterToMultiValueMap(null, "sum", sum));
+        
+        final String[] localVarAccepts = { 
+            "application/json"
+        };
+        final List<MediaType> localVarAccept = apiClient.selectHeaderAccept(localVarAccepts);
+        final String[] localVarContentTypes = { };
+        final MediaType localVarContentType = apiClient.selectHeaderContentType(localVarContentTypes);
+
+        String[] localVarAuthNames = new String[] {  };
+
+        ParameterizedTypeReference<PaymentResponse> localVarReturnType = new ParameterizedTypeReference<PaymentResponse>() {};
+        return apiClient.invokeAPI("/users/{userId}/makePayment", HttpMethod.POST, pathParams, queryParams, postBody, headerParams, cookieParams, formParams, localVarAccept, localVarContentType, localVarAuthNames, localVarReturnType);
+    }
+
+    /**
+     * Совершить платеж (совершить списание денег со счёта)
+     * 
+     * <p><b>200</b> - Успешный ответ
+     * <p><b>400</b> - Превышена максимально возможная сумма платежа
+     * <p><b>404</b> - Пользователь не найден
+     * <p><b>5XX</b> - Внутренняя ошибка сервера
+     * <p><b>0</b> - Неизвестная ошибка при выполнении платежа
+     * @param sum The sum parameter
+     * @param userId Идентификатор пользователя
+     * @return PaymentResponse
+     * @throws WebClientResponseException if an error occurs while attempting to invoke the API
+     */
+    public Mono<PaymentResponse> usersUserIdMakePaymentPost(Float sum, Integer userId) throws WebClientResponseException {
+        ParameterizedTypeReference<PaymentResponse> localVarReturnType = new ParameterizedTypeReference<PaymentResponse>() {};
+        return usersUserIdMakePaymentPostRequestCreation(sum, userId).bodyToMono(localVarReturnType);
+    }
+
+    /**
+     * Совершить платеж (совершить списание денег со счёта)
+     * 
+     * <p><b>200</b> - Успешный ответ
+     * <p><b>400</b> - Превышена максимально возможная сумма платежа
+     * <p><b>404</b> - Пользователь не найден
+     * <p><b>5XX</b> - Внутренняя ошибка сервера
+     * <p><b>0</b> - Неизвестная ошибка при выполнении платежа
+     * @param sum The sum parameter
+     * @param userId Идентификатор пользователя
+     * @return ResponseEntity&lt;PaymentResponse&gt;
+     * @throws WebClientResponseException if an error occurs while attempting to invoke the API
+     */
+    public Mono<ResponseEntity<PaymentResponse>> usersUserIdMakePaymentPostWithHttpInfo(Float sum, Integer userId) throws WebClientResponseException {
+        ParameterizedTypeReference<PaymentResponse> localVarReturnType = new ParameterizedTypeReference<PaymentResponse>() {};
+        return usersUserIdMakePaymentPostRequestCreation(sum, userId).toEntity(localVarReturnType);
+    }
+
+    /**
+     * Совершить платеж (совершить списание денег со счёта)
+     * 
+     * <p><b>200</b> - Успешный ответ
+     * <p><b>400</b> - Превышена максимально возможная сумма платежа
+     * <p><b>404</b> - Пользователь не найден
+     * <p><b>5XX</b> - Внутренняя ошибка сервера
+     * <p><b>0</b> - Неизвестная ошибка при выполнении платежа
+     * @param sum The sum parameter
+     * @param userId Идентификатор пользователя
+     * @return ResponseSpec
+     * @throws WebClientResponseException if an error occurs while attempting to invoke the API
+     */
+    public ResponseSpec usersUserIdMakePaymentPostWithResponseSpec(Float sum, Integer userId) throws WebClientResponseException {
+        return usersUserIdMakePaymentPostRequestCreation(sum, userId);
+    }
+}
